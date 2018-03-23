4edfd59ac9a827adff0e0b9c03609b450e2ce0d4
numbahs <panyba@gmail.com>
2018-03-23 17:44:15 -0400

    for merlin, also started subs.ml

--- HW1/infer.ml
+++ HW1/infer.ml
@@ -9,10 +9,30 @@ type typing_judgement = subst*expr*texpr
 
 let rec infer' (e:expr) (n:int): (int*typing_judgement) error =
   match e with
-  | _ -> failwith "infer': undefined"
+  | Unit  -> failwith "undefined"
+  | Var s -> failwith "undefined"
+  | Int n -> failwith "undefined"
+  | Add(e1,e2) -> failwith "undefined"
+  | Sub(e1,e2) -> failwith "undefined"
+  | Mul(e1,e2) -> failwith "undefined"
+  | Div(e1,e2) -> failwith "undefined"
+  | NewRef(e) -> failwith "undefined"
+  | DeRef(e) -> failwith "undefined"
+  | SetRef(e1,e2) -> failwith "undefined"
+  | Let(x,def,body) -> failwith "undefined"
+  | Proc(x,t,body) -> failwith "undefined"
+  | ProcUntyped(x,body) -> failwith "undefined"
+  | App(e1,e2) -> failwith "undefined"
+  | IsZero(e) -> failwith "undefined"
+  | ITE(e1,e2,e3) -> failwith "undefined"
+  | Letrec(tRes,x,param,tPara, def,body) -> failwith "undefined"
+  | LetrecUntyped(x,param,def,body) -> failwith "undefined"
+  | Set(x,rhs) -> failwith "undefined"
+  | BeginEnd(es) -> failwith "undefined"
+  | _ -> failwith "infer: undefined"
 
 let string_of_typing_judgement (sub, expr, texpr) = 
-    "(" ^ (Subs.string_of_subs sub) ^ "), " ^ (Ast.string_of_expr expr) ^ ", " ^ (Ast.string_of_texpr texpr) ^ ")"
+  "(" ^ (Subs.string_of_subs sub) ^ "), " ^ (Ast.string_of_expr expr) ^ ", " ^ (Ast.string_of_texpr texpr) ^ ")"
 
 
 let infer_type (AProg e) =
--- HW1/subs.ml
+++ HW1/subs.ml
@@ -1,11 +1,18 @@
 open Ast
 
-type subst = (string,Ast.texpr) Hashtbl.t
+type subst = (string,texpr) Hashtbl.t
+
+let create:subst = Hashtbl.create 50
+
+let extend (s:subst) (x:string) (t:texpr):unit = Hashtbl.add s x t
+
+let remove (s:subst) (x:string):unit = Hashtbl.remove s x
+
+let lookup (s:subst) (x:string):(Ast.texpr option) = Hashtbl.find_opt s x
 
 let rec string_of_subst_pairs = function
-    | [] -> ""
-    | [(k, v)] -> "(" ^ k ^ ", " ^ (Ast.string_of_texpr v) ^ ")"
-    | (k, v)::rest -> "(" ^ k ^ ", " ^ (Ast.string_of_texpr v) ^ "), " ^ (string_of_subst_pairs rest)
+  | [] -> ""
+  | [(k, v)] -> "(" ^ k ^ ", " ^ (Ast.string_of_texpr v) ^ ")"
+  | (k, v)::rest -> "(" ^ k ^ ", " ^ (Ast.string_of_texpr v) ^ "), " ^ (string_of_subst_pairs rest)
 
 let string_of_subs t = string_of_subst_pairs @@ Hashtbl.fold(fun k v acc -> (k, v)::acc) t []
-
