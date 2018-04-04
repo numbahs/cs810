5b29a0c4f5de3bd32c1ef0744d7e7ab8bed874ef
Robert Herley <rherley@stevens.edu>
2018-04-03 20:01:36 -0400

    added all tests, making new ones now

--- HW1/tests.ml
+++ HW1/tests.ml
@@ -8,37 +8,30 @@ let inf (s:string) : string =
   s |> parse |> infer_type
 
 (* Testing expression inference *)
-let expr_test (n:int) : unit =
-  expr_cases n |> inf |> print_string
+let expr_test (n:int) : string =
+  expr_cases n |> inf 
+
+(* Expression inference tests that should fail *)
+let fail_test (n:int) : string =
+  fail_cases n |> inf 
 
 (* Testing mgu unification *)
-let mgu_test (n:int) =
+let mgu_test (n:int) : string =
   mgu_cases n |> mgu |> string_of_mgu
 
 (* Testing joining of type env *)
-let join_test (n:int) = 
+let join_test (n:int) : string = 
   join_cases n |> string_of_subs
 
-(* let print_tests () = 
-   for i = 1 to 2 do
-    print_string @@ string_of_int i ^ " " ^ join_tests i;
-    print_string "\n";
-   done;
-   for i=1 to 7 do
-    print_string @@ string_of_int i ^ " " ^ mgu_tests i;
-    print_string "\n";
-   done;
-   for i=1 to 26 do
-    print_string @@ "\027[30m" ^ string_of_int i ^ " " ^ (inf @@ Examples.expr i);
-    print_string "\n";
-   done;
-   print_string @@ "---------------------------------------------------- SHOULD FAIL -------------------------------------------------------------";
-   print_string "\n";
-   for i=1 to 10 do
-    print_string @@ "\027[30m" ^ string_of_int i ^ " " ^ (inf @@ Examples.should_fail i);
-    print_string "\n";
-   done; *)
+(* Helper func to print tests *)
+let test_helper (fn) (str: string) (n:int) : unit =
+  Printf.printf "---| %s Tests |---\n" str;
+  for i = 1 to n do
+    Printf.printf ("\027[30m %d: \027[32m%s\n\027[39m") i (fn i) ;
+  done;;
 
-(* let run_tests = function 
-   | "test" -> print_string @@ inf "0"
-   | _ -> failwith "you goofed" *)
+let run_tests () : unit = 
+  test_helper expr_test "Expression" 26;
+  test_helper fail_test "Fail" 10;
+  test_helper join_test "Join" 2;
+  test_helper mgu_test "MGU" 7;;
